da0de76b8b2f03697ef59215cbbbac476fb736ad
Zeba Momin <mominz2112@gmail.com>
2026-09-19 18:55:00 +0530

    Map Maker & Popup

--- public/js/map.js
+++ public/js/map.js
@@ -12,7 +12,7 @@ const coordinates = listing.geometry.coordinates;
 
 const map = new maptilersdk.Map({
   container: "map",
-  style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${mapKey}`,
+  style: maptilersdk.MapStyle.STREETS, //`https://api.maptiler.com/maps/streets-v2/style.json?key=${mapKey}`,
   center: coordinates,
   zoom: 9,
 });
@@ -21,12 +21,24 @@ const map = new maptilersdk.Map({
 map.addControl(new maptilersdk.NavigationControl(), "top-right");
 
 // Marker + Popup
-const popup = new maptilersdk.Popup({ offset: 25 }).setHTML(
-  `<h4>${listing.title}</h4>
-   <p>Exact location will be provided after booking</p>`,
-);
 
-new maptilersdk.Marker({ color: "#fe424d" })
+const marker = new maptilersdk.Marker({ color: "#fe424d" })
   .setLngLat(coordinates)
-  .setPopup(popup)
+  .setPopup(
+    new maptilersdk.Popup({ offset: 25 })
+      .setHTML(
+        `<h5>${listing.title}</h5><p>Exact location will be provided after booking</p>`,
+      )
+      .setMaxWidth("300px"),
+  )
   .addTo(map);
+
+// const popup = new maptilersdk.Popup({ offset: 25 }).setHTML(
+//   `<h4>${listing.title}</h4>
+//    <p>Exact location will be provided after booking</p>`,
+// );
+
+// new maptilersdk.Marker({ color: "#fe424d" })
+//   .setLngLat(coordinates)
+//   .setPopup(popup)
+//   .addTo(map);
